95517124ea871388bcdf9f5245d80a4617561664
Neptius <aymeric.legros@gmail.com>
2025-02-27 21:52:10 +0100

    up

--- config/config.exs
+++ config/config.exs
@@ -1,3 +1,6 @@
 import Config
 
-config :novy_core, ecto_repos: [NovyCore.Repo]
+config :novy_core,
+  ecto_repos: [NovyCore.Repo]
+
+config :novy_core, Novy.Mailer, adapter: Swoosh.Adapters.Local
--- priv/repo/migrations/20250227191111_toto.exs
+++ priv/repo/migrations/20250227191111_toto.exs
@@ -2,6 +2,5 @@ defmodule NovyCore.Repo.Migrations.Toto do
   use Ecto.Migration
 
   def change do
-
   end
 end
