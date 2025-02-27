9a5ad9afde547a446e85751a527df2cfe309ec18
Aymeric <aymeric.legros@adixon.fr>
2025-02-27 17:16:13 +0100

    todo

--- lib/novy_core.ex
+++ lib/novy_core.ex
@@ -2,17 +2,4 @@ defmodule NovyCore do
   @moduledoc """
   Documentation for `NovyCore`.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> NovyCore.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
