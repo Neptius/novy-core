22b7696c46610da403c4aaa1e986fe475f39c11d
Neptius <aymeric.legros@gmail.com>
2025-03-13 20:30:51 +0100

    up

--- config/dev.secret.example.exs
+++ config/dev.secret.example.exs
@@ -4,9 +4,9 @@ config :novy_core,
   stratz_api_key: ""
 
 config :novy_core, NovyCore.Repo,
-  database: "novy_core_repo",
+  database: "novy_dev",
   username: "postgres",
-  password: "pass",
+  password: "password",
   hostname: "localhost",
   stacktrace: true,
   show_sensitive_data_on_connection_error: true,
